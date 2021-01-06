d8f23603fedecf29e54c3a9a8fc1ff7a51479028
Diego Lira <diegoalira@gmail.com>
2021-01-06 11:52:49 -0300

    Save project to local storage

--- src/classes/projects.js
+++ src/classes/projects.js
@@ -1,3 +1,22 @@
-const Project = (title, description, priority) => ({ title, description, priority });
+const Project = (title, description, priority) => {
+  return { title, description, priority };
+};
 
-export default Project;
+const getFormValues = () => {
+  const valuesForm = document.querySelectorAll('input');
+  const values = [];
+  for (let i = 0; i < valuesForm.length; i++) {
+    values.push(valuesForm[i].value);
+  }
+
+  let b = JSON.parse(window.localStorage.getItem('projects'));
+  let p = Project(values[0], values[1], values[2]);
+  b.push(p)
+  window.localStorage.setItem('projects', [JSON.stringify(b)]);
+
+  
+};
+
+
+
+export { Project, getFormValues };
--- src/interface/new-project.js
+++ src/interface/new-project.js
@@ -1,18 +1,12 @@
 /* eslint-disable no-unused-vars */
-import Project from '../classes/projects';
+import { Project, getFormValues } from '../classes/projects';
 
 const newProject = () => {
-  const getFormValues = () => {
-    const valuesForm = document.querySelectorAll('inputs');
-    const values = [];
-    for (let i = 0; i < valuesForm.length; i = +1) {
-      values.push(valuesForm[i].value);
 
-    }
-    const p = Project(values);
-    console.log(p);
-    return p;
-  };
+  document.addEventListener('DOMContentLoaded', (event) => {
+    const el = document.getElementById('#button');
+    el.addEventListener('click', () => getFormValues());
+  });
 
   const html = `
     <form class="block text-sm font-medium text-gray-700 w-8/12">
@@ -23,17 +17,17 @@ const newProject = () => {
       </div>
       <label> Project Description </label>
       <div class="mt-1 relative rounded-md shadow-sm border-gray-800">
-        <input type="text" name="project-title" placeholder="Type your project name" class="focus:ring-indigo-500 
+        <input type="text" name="project-description" placeholder="Type your project name" class="focus:ring-indigo-500 
         focus:border-indigo-500 block w-full pl-7 pr-12 sm:text-sm  rounded-md h-12">
       </div>
       <label> Project Priority </label>
       <div class="mt-1 relative rounded-md shadow-sm border-gray-800">
-        <input type="text" name="project-title" placeholder="Type your project name" class="focus:ring-indigo-500 
+        <input type="text" name="project-priority" placeholder="Type your project name" class="focus:ring-indigo-500 
         focus:border-indigo-500 block w-full pl-7 pr-12 sm:text-sm  rounded-md h-12">
       </div>
-      <button onclick ="getFormValues"  class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full">
+      <div id="#button" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full">
           Create project
-      </button>
+      </div>
     </form>
   `;
 
